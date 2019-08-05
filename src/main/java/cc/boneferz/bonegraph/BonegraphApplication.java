dd8072e73445a96e4e2c22abdfeeac0537b3bc5d
boneferz <boneferz@gmail.com>
2019-08-05 19:49:14 +0300

    init

---
+++ src/main/java/cc/boneferz/bonegraph/BonegraphApplication.java
@@ -0,0 +1,13 @@
+package cc.boneferz.bonegraph;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BonegraphApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(BonegraphApplication.class, args);
+	}
+	
+}
