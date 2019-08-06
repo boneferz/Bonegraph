a1e5f857d7f52a2cc269c89c62cc0b1317e70629
boneferz <boneferz@gmail.com>
2019-08-06 06:56:02 +0300

    update

---
+++ src/test/java/cc/boneferz/bonegraph/BonegraphApplicationTests.java
@@ -0,0 +1,16 @@
+package cc.boneferz.bonegraph;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BonegraphApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
